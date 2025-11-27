b33100878c1f73333d9d13229d6e69fef1c85dad
Fernando <Fernando.Work.Account@proton.me>
2025-11-27 18:53:21 +0100

    Modified button

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -89,7 +89,7 @@
                 </div>
 
                 <button class="bg-blue-500 text-white px-4 py-2 rounded">
-                    Guardar
+                    Guardar tarea
                 </button>
             </form>
         </div>
